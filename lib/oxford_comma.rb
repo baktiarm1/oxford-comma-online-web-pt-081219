ec7163b9145b0199a50a5d94fd4ed599a7677c2a
Baktiar Miah <baktiar.m@gmail.com>
2019-08-23 19:40:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,14 @@
 def oxford_comma(arr)
-  if arr.size===1
+  if arr.size==1
     return arr[0]
-  else if arr.size ==2
-  return arr[0] + ' and ' + arr[1] 
- else arr.size>2
+  else if arr.size == 2
+    return arr[0] + ' and ' + arr[1] 
+    
+  else
     lword = arr[arr.size-1] 
     lword = 'and '+lword
     arr[arr.size-1] = lword
     return arr.join(', ')
+    end
   end
-    
 end
